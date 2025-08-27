30e66d98df2f3af59afd8e08fa426d1ea442aafc
NELSON OTIKA <138373301+otikanelson@users.noreply.github.com>
2025-08-27 02:05:55 +0100

    dployment fixes

--- Darb-Frontend/src/main.jsx
+++ Darb-Frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { initializeApp } from './Services/AppInitializer.js'
+import { initializeApp } from './services/AppInitializer.js'
 
 initializeApp();
 
--- Darb-Frontend/src/pages/MyCampaigns.jsx
+++ Darb-Frontend/src/pages/MyCampaigns.jsx
@@ -5,7 +5,7 @@ import { useAuth } from "../context/AuthContext";
 import UnifiedNavbar from "../components/layout/Navbars";
 import Footer from "../components/layout/Footer";
 import CampaignCard from "../components/ui/CampaignCard";
-import CampaignService from "../services/CampaignService";
+import CampaignService from "../services/campaignService";
 import {
   Eye,
   Heart,
