5f331aba470eae4d0730ff213cdf2958a1b7b232
Otika Nelson <otikanelson29@email.com>
2025-10-21 04:11:32 +0100

    db config omission fix

---
+++ backend/config/db.config.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+module.exports = {
+  HOST: process.env.DB_HOST || "localhost",
+  USER: process.env.DB_USER || "root",
+  PASSWORD: process.env.DB_PASSWORD || "RADson292005", 
+  DB: process.env.DB_NAME || "darb_network_db",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
